c09f86725d2c1f9e2100f777cf9dddae47ab9647
Arnau Zapata Delgado <azapata@Arnaus-MacBook-Pro-8.local>
2024-11-09 18:20:37 +0100

    remove redirection

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
 // Middleware para parsear JSON
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('¡Hola desde el backend de Node.js!');
-});
+  res.send('¡Hola desde el backend de Node.js!')
+})
 
 app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+  console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
 
-const stripe = require('stripe')(process.env.API_KEY_STRIPE);
-
+const stripe = require('stripe')(process.env.API_KEY_STRIPE)
 
 app.post('/payment', async (req, res) => {
   try {
-    const { amount, currency, paymentMethodId } = req.body;
+    const { amount, currency, paymentMethodId } = req.body
 
     // Crear un pago con Stripe
     const paymentIntent = await stripe.paymentIntents.create({
@@ -27,10 +26,14 @@ app.post('/payment', async (req, res) => {
       currency,
       payment_method: paymentMethodId,
       confirm: true,
-    });
+      automatic_payment_methods: {
+        enabled: true,
+        allow_redirects: 'never',
+      },
+    })
 
-    res.status(200).json({ success: true, paymentIntent });
+    res.status(200).json({ success: true, paymentIntent })
   } catch (error) {
-    res.status(500).json({ success: false, error: error.message });
+    res.status(500).json({ success: false, error: error.message })
   }
-});
+})
